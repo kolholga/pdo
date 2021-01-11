cc5aeedad2a9a94d4d481989690349c112ebbe35
Dmitriy Potapenko <dmorskoi@yahoo.com>
2021-01-11 21:41:48 +0300

    Теория

---
+++ books.sql
@@ -0,0 +1,68 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Хост: 127.0.0.1:3306
+-- Время создания: Янв 11 2021 г., 21:39
+-- Версия сервера: 10.3.22-MariaDB
+-- Версия PHP: 7.1.33
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- База данных: `pdo`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `books`
+--
+
+CREATE TABLE `books` (
+  `id` int(11) NOT NULL,
+  `title` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `author` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `year` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Дамп данных таблицы `books`
+--
+
+INSERT INTO `books` (`id`, `title`, `author`, `year`) VALUES
+(1, 'Война и мир', 'Л.Толстой', '2007'),
+(2, 'Мастер и Маргарита', 'Булгаков', '2005');
+
+--
+-- Индексы сохранённых таблиц
+--
+
+--
+-- Индексы таблицы `books`
+--
+ALTER TABLE `books`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT для сохранённых таблиц
+--
+
+--
+-- AUTO_INCREMENT для таблицы `books`
+--
+ALTER TABLE `books`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ index.php
@@ -0,0 +1,66 @@
+<?php
+
+//ТЕОРИЯ по PDO
+/*
+//https://www.php.net/manual/ru/book.pdo.php
+// PDO - специальный модуль в PHP,
+// который предоставляет удобный интерфейс для доступа к базам данных
+// PDO - это класс
+// обязательные аргументы:
+// 1. dsn - это строка в формате:
+//   <тип драйвера>:host=<хост>;dbname=<имя базы>
+// 2. имя пользователя БД
+// 3. пароль БД
+*/
+
+try { //исключение
+    $pdo = new PDO('mysql:host=localhost;dbname=pdo', 'root', 'root'); // объект подключения
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+
+
+
+} catch (PDOException $e) {
+    echo $e->getMessage();
+    die();
+}
+
+/*
+$res = $pdo->query('SELECT * FROM `books`'); //выполняет запрос к БД
+
+// $res->fetchAll() - возвращает массив из пришедших данных
+
+//echo '<pre>';
+//print_r($res->fetchAll(PDO::FETCH_ASSOC)); // атрибутом указываем тип данных, который нужно вернуть
+//echo '</pre>';
+
+
+//    print_r($res->fetch(PDO::FETCH_ASSOC)); // fetch() - возвращает только одну запись и СДВИГАЕТ указатель
+//    print_r($res->fetch(PDO::FETCH_ASSOC));
+//    var_dump($res->fetch(PDO::FETCH_ASSOC));
+// напрашивается цикл while:
+while ($arRes = $res->fetch(PDO::FETCH_ASSOC)) {
+    echo 'Книга - ' . $arRes['title'] . '<br>';
+    echo 'Автор - ' . $arRes['author'] . '<br>';
+    echo '<br><hr><br>';
+}
+
+//$arr = PDO::getAvailableDrivers();
+
+//echo '<pre>';
+//print_r($arr);
+//echo '</pre>';
+*/
+
+$id = 2;
+//$stm = $pdo->prepare("SELECT * FROM `books` WHERE `id` = ?"); //prepare - позволяет использовать плейсхолдер (в query - нельзя)
+//$stm->execute([$id]);
+$stm = $pdo->prepare("SELECT * FROM `books` WHERE `id` = :id AND `year` = :year"); // именованный плейсхолдер
+$stm->execute([':id' => $id, ':year' => 2005]); // когда именованный плейсхолдер, то порядок не имеет значения
+
+$res = $stm->fetch(PDO::FETCH_ASSOC);
+
+print_r($res);
+
+$cntUpdate = $pdo->exec("UPDATE `books` SET `year` = 2007  WHERE `id` = 1"); //Возвращает количество затронутых записей
+var_dump($cntUpdate);
